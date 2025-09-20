bfb7ea1ec2c3e788999176e095f1c14100135347
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-09-20 19:38:01 +0500

    add(Patient-Entity): a new table has been added named as entity

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -1,11 +1,15 @@
 package com.pm.patientservice.model;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Patient{
@@ -14,8 +18,21 @@ public class Patient{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    
+    @NotNull
+    private String name;
 
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
 
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
 
 }
