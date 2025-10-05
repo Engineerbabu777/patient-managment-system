c6b74c07efe2ae4cfc7aa13c4ed47498365f5543
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-10-05 17:48:39 +0500

    fix(EmailAlreadyExistsException): this class has been adjusted again

--- patient-service/src/main/java/com/pm/patientservice/exception/EmailAlreadyExistsException.java
+++ patient-service/src/main/java/com/pm/patientservice/exception/EmailAlreadyExistsException.java
@@ -2,7 +2,9 @@ package com.pm.patientservice.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
 
-    public EmailAlreadyExistsException() {
+    public EmailAlreadyExistsException(String messae) {
+        super(messae);
+
     }
 
 }
