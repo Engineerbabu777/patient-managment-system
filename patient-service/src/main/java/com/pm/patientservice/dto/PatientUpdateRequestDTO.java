628c0e70f4ae9af7b968cdb97e3b5da810e80ee6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-10-05 20:04:31 +0500

    add(PatientUpdateRequestDTO): all getter and setters has been added

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientUpdateRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientUpdateRequestDTO.java
@@ -2,4 +2,42 @@ package com.pm.patientservice.dto;
 
 public class PatientUpdateRequestDTO {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String email;
+    private String dateOfBirth;
+    private String address;
+
 }
